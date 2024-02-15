efb180a6d3fe1fb1d3a832fe6ee151760f1a508c
josue hernandez <josueahs21@gmail.com>
2024-02-15 16:33:14 -0300

    feat: update portfolio README.md

---
+++ js/animation.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.menu_header' , {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.redes' , {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+  /*  sr.reveal('.acerca_de_mi' , {
+        duration: 4000,
+        origin: 'left',
+        distance: '400px'
+    });
+
+    sr.reveal('.skills' , {
+        duration: 4000,
+        origin: 'rigth',
+        distance: '100px'
+    });
+
+    sr.reveal('.hobbies' , {
+        duration: 4000,
+        origin: 'left',
+        distance: '100px'
+    });*/
